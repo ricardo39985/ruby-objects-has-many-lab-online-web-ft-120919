34081b5451c17d134f90dd404454065e2073f1d2
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-27 19:25:19 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -25,7 +25,7 @@ class Author
     @@all
   end
   def add_post(post_title)
-    new_post, new_post.author = post_title, self
+    new_post, new_post.author = post_title, self.name
 
   end
   def add_post_by_title(post_title)
